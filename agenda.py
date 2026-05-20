4810098223341bbcb6a1d3a81e1ed7ad08b3aae2
Hillary Moura <161659454+hnmoura@users.noreply.github.com>
2026-05-20 19:12:54 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -1,9 +1,1 @@
-agenda = {}
-
-def adicionar_contato():
-    nome = input("Nome: ").strip()
-    telefone = input("Telefone: ").strip()
-    email = input("E-mail: ").strip()
-    agenda[nome] = {"telefone": telefone, "email": email}
-    print(f"\nContato '{nome}' adicionado com sucesso!")
 
